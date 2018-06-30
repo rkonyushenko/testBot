3d4dbaf76c69716b96e50651a2a25f2e79d05ef2
Roman <rkonyushenko@softwear.finance>
2018-06-30 15:03:20 +0300

    test

--- Bot.js
+++ Bot.js
@@ -7,7 +7,7 @@ const TOKEN = config.token;
 const openKeyboard = {
     reply_markup: {
         keyboard: [
-            ["Да, буду", "Нахуй пішов"],
+            ["Давай", "Ні, давай без мене"],
         ],
         one_time_keyboard: true,
         resize_keyboard: true,
@@ -18,21 +18,19 @@ const bot = new TelegramBot(TOKEN);
 Server.createServer(bot);
 bot.setWebHook(`${config.url}/bot`);
 
+// if (username === 'K_Sergey_V' || username === 'llemo'){
+//     bot.sendMessage(id, `${msg.from.first_name}, давай загранку робить, заїбав!!!`);
+// } else if (username === 'V_Yarosh') {
+//     bot.sendMessage(id, `${msg.from.first_name}, хвате катать в доту, давай загранку роби!`);
+//
+
 bot.on('message', msg => {
     const {chat: {id}} = msg;
     const {chat: {username}} = msg;
+    const {text} = msg;
     console.log(JSON.stringify(msg));
-    if (username === 'K_Sergey_V' || username === 'llemo'){
-        bot.sendMessage(id, `${msg.from.first_name}, давай загранку робить, заїбав!!!`);
-    } else if (username === 'V_Yarosh') {
-        bot.sendMessage(id, `${msg.from.first_name}, хвате катать в доту, давай загранку роби!`);
-    }
-        bot.sendMessage(id, `Півос?`, openKeyboard);
-        if (msg.text === 'Нахуй пішов') {
-            bot.sendSticker(id, 'CAADAgADOAADX8p-CzLiVfbJsCagAg')
-        } else {
-            bot.sendSticker(id,'CAADBAADygIAAlI5kwY1OpJy7RHINwI')
-            bot.sendMessage(id,'Щас Влад базу підтягне, жди...)))')
-        }
-
+    if (text === '/start') {
+        bot.sendMessage(id, 'Привіт, я алкобот, я ще тупий, і мою базу буде піднімать Влад, але ти можеш мені писать.)')
+        bot.sendMessage(id, 'Може ти хочеш щось затестить?', openKeyboard)
+    } else {}
 });
