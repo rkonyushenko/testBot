3205bbfa24f50677c7472d8a6ba4b7d86d93be05
Roman <rkonyushenko@softwear.finance>
2018-06-30 20:45:24 +0300

    build

--- bot/BotClass.js
+++ bot/BotClass.js
@@ -62,7 +62,7 @@ class BotHelper {
                     db.selectOne(condition)
                         .then(result => {
                             console.log([{text: `Спробуй ${result.name}`}]);
-                            resolve ([{text: `Спробуй ${result.name}`}])
+                            resolve ([{text: `Спробуй ${result}`}])
                         })
                         .catch(err => reject(err));
                 });
