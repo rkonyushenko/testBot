cbcb60e873272fedb651f532b7fd4c64accaf9f3
Roman <rkonyushenko@softwear.finance>
2018-07-01 01:00:46 +0300

    build

--- Bot.js
+++ Bot.js
@@ -29,16 +29,8 @@ bot.on('message', msg => {
     if (text === '/start') {
         bot.sendMessage(id, 'Привіт, я алкобот, я ще тупий, і мою базу буде піднімать Влад, але ти можеш мені писать.)');
         bot.sendMessage(id, 'Може ти хочеш щось затестить?', openKeyboard);
-    } else {
-        return new Promise((resolve, reject) => {
-            botHelper.checkButtonPressed(text)
-                .then(result => {
-                    bot.sendMessage(id, JSON.stringify(result[0]['text']), result[1]);
-                })
-                .catch(err => reject(err))
-
-        })
     }
+
     eventEmitter.on('next', () => {
         return new Promise((resolve, reject) => {
             db.selectNext({type: 'ale'}, true)
@@ -62,6 +54,73 @@ bot.on('message', msg => {
         };
         bot.sendMessage(id, 'ммм, півос))) вибирай)', keys)
     });
+
+    eventEmitter.on('Давай', () => {
+        const keys = {
+            reply_markup: {
+                keyboard: [
+                    ["Пиво", "Ель"],
+                ],
+                one_time_keyboard: true,
+                resize_keyboard: true
+            }
+        };
+        bot.sendMessage(id, 'Класно, вибирай', keys)
+    });
+
+    eventEmitter.on('Давай', () => {
+        const keys = {
+            reply_markup: {
+                keyboard: [
+                    ["Прощай"],
+                ],
+                one_time_keyboard: true,
+                resize_keyboard: true
+            }
+        };
+        bot.sendMessage(id, 'До зустрічі', keys)
+    });
+
+    eventEmitter.on('Світле', () => {
+        return new Promise((resolve, reject) => {
+            const condition = {type: 'beer',isLight: true};
+            db.selectOne(condition)
+                .then(result => {
+                    console.log([{text: `Спробуй ${result.name}`}]);
+                    bot.sendMessage(id, `Спробуй ${result.name}  ${result.description}`)
+                })
+                .catch(err => reject(err));
+        });
+    });
+
+    eventEmitter.on('Темне', () => {
+        return new Promise((resolve, reject) => {
+            const condition = {type: 'beer',isDark: true};
+            db.selectOne(condition)
+                .then(result => {
+                    console.log([{text: `Спробуй ${result.name}`}]);
+                    bot.sendMessage(id, `Спробуй ${result.name}  ${result.description}`)
+                })
+                .catch(err => reject(err));
+        });
+    });
+
+    eventEmitter.on('Ель', () => {
+        return new Promise((resolve, reject) => {
+            const condition = {type: 'ale'};
+            db.selectOne(condition)
+                .then(result => {
+                    console.log([{text: `Спробуй ${result.name}`}]);
+                    resolve ([{text: `Спробуй ${result.name}  ${result.description}`}, {reply_markup: {
+                            inline_keyboard: [
+                                [{text: 'Next', callback_data: 'next'}]
+                            ]
+                        }}])
+                })
+                .catch(err => reject(err));
+        });
+    });
+
 });
 const Emitter = require('events');
 const eventEmitter = new Emitter();
--- bot/BotClass.js
+++ bot/BotClass.js
@@ -1,89 +1,30 @@
-const db = require('./../Mongo');
-
-
-class BotHelper {
-
-    checkButtonPressed(btn) {
-        switch (btn) {
-            case 'Давай':
-                return new Promise((resolve, reject) => {
-                    resolve([{text: 'Класно, вибирай'}, {
-                        reply_markup: {
-                            keyboard: [
-                                ["Пиво", "Ель"],
-                            ],
-                            one_time_keyboard: true,
-                            resize_keyboard: true
-                        }
-                    }]);
-                });
-
-            case 'Ні, давай без мене':
-                return [{text: 'Прощай'}, {
-                    reply_markup: {
-                        keyboard: [
-                            ["Прощай"],
-                        ],
-                        one_time_keyboard: true,
-                        resize_keyboard: true
-                    }
-                }];
-
-
-
-            case 'Світле':
-                return new Promise((resolve, reject) => {
-                    const condition = {type: 'beer',isLight: true};
-                    db.selectOne(condition)
-                        .then(result => {
-                            console.log([{text: `Спробуй ${result.name}`}]);
-                            resolve ([{text: `Спробуй ${result.name}  ${result.description}`}])
-                        })
-                        .catch(err => reject(err));
-                });
-
-            case 'Темне':
-                return new Promise((resolve, reject) => {
-                    const condition = {type: 'beer',isDark: true};
-                    db.selectOne(condition)
-                        .then(result => {
-                            console.log([{text: `Спробуй ${result.name}`}]);
-                            resolve ([{text: `Спробуй ${result.name}  ${result.description}`}])
-                        })
-                        .catch(err => reject(err));
-                });
-
-            case 'Ель':
-                return new Promise((resolve, reject) => {
-                    const condition = {type: 'ale'};
-                    db.selectOne(condition)
-                        .then(result => {
-                            console.log([{text: `Спробуй ${result.name}`}]);
-                            resolve ([{text: `Спробуй ${result.name}  ${result.description}`}, {reply_markup: {
-                                    inline_keyboard: [
-                                        [{text: 'Next', callback_data: 'next'}]
-                                    ]
-                                }}])
-                        })
-                        .catch(err => reject(err));
-                });
-
-            default:
-                return new Promise((resolve, reject) => {
-                    resolve([{text: 'оххх, давай почнемо з початку'}, {
-                        reply_markup: {
-                            keyboard: [
-                                ["/start"],
-                            ],
-                            one_time_keyboard: true,
-                            resize_keyboard: true
-                        }
-                    }])
-                });
-        }
-    }
-}
-
-const
-    botHelper = new BotHelper();
-module.exports = botHelper;
+// const db = require('./../Mongo');
+//
+//
+// class BotHelper {
+//
+//     checkButtonPressed(btn) {
+//         switch (btn) {
+//
+//             case 'Ель':
+//
+//
+//             default:
+//                 return new Promise((resolve, reject) => {
+//                     resolve([{text: 'оххх, давай почнемо з початку'}, {
+//                         reply_markup: {
+//                             keyboard: [
+//                                 ["/start"],
+//                             ],
+//                             one_time_keyboard: true,
+//                             resize_keyboard: true
+//                         }
+//                     }])
+//                 });
+//         }
+//     }
+// }
+//
+// const
+//     botHelper = new BotHelper();
+// module.exports = botHelper;
