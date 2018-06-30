01466c8505d4282ecba15cec87ed8948c59a0801
Roman <rkonyushenko@softwear.finance>
2018-06-30 13:10:45 +0300

    url change

--- Mongo.js
+++ Mongo.js
@@ -1,20 +1,20 @@
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = `mongodb://lemo:qwerty123@ds161610.mlab.com:61610/botdb_alcho`;
-
-class MongoDB {
-
-    getConnection() {
-            MongoClient.connect(uri, function(err, client) {
-                if (err) throw err;
-               const collection = client.db("botdb_alcho").collection('alchoCollection');
-               collection.insert({key: 'value'})
-               console.log(collection)
-             
-            });  
-        
-    }
-}
-
-const mongo = new MongoDB();
-module.exports = mongo;
+// const MongoClient = require('mongodb').MongoClient;
+//
+// const uri = `mongodb://lemo:qwerty123@ds161610.mlab.com:61610/botdb_alcho`;
+//
+// class MongoDB {
+//
+//     getConnection() {
+//             MongoClient.connect(uri, function(err, client) {
+//                 if (err) throw err;
+//                const collection = client.db("botdb_alcho").collection('alchoCollection');
+//                collection.insert({key: 'value'})
+//                console.log(collection)
+//
+//             });
+//
+//     }
+// }
+//
+// const mongo = new MongoDB();
+// module.exports = mongo;
